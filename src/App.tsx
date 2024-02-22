48315895022a40a0172d22feca6edb6be74a280b
Nikita Kolesnew <chattrain.kol7@gmail.com>
2024-02-22 18:48:56 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Route, Routes } from "react-router-dom";
+
+import AuthLayout from "./_auth/AuthLayout";
+import SigininForm from "./_auth/forms/SigininForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
+
+import "./globals.css";
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/*public routes*/}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigininForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/*private routes*/}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/_auth/forms/SigininForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SigininForm = () => {
+  return (
+    <div>SigininForm</div>
+  )
+}
+
+export default SigininForm
