742b1eb6914fad0fdf8682a4562abeeb6c524a03
Nikita Kolesnew <chattrain.kol7@gmail.com>
2024-02-26 14:04:01 +0300

    feat: added typing for posts

--- src/types/index.ts
+++ src/types/index.ts
@@ -37,3 +37,22 @@ export type INewUser = {
   username: string;
   password: string;
 };
+
+
+export type INewPost = {
+  userId: string;
+  caption: string;
+  file: File[];
+  location?: string;
+  tags?: string;
+};
+
+export type IUpdatePost = {
+  postId: string;
+  caption: string;
+  imageId: string;
+  imageUrl: URL;
+  file: File[];
+  location?: string;
+  tags?: string;
+};
