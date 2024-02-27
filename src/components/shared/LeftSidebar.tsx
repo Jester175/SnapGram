52e2b12ad67e2f46fecafc378a92b76c09739b32
Nikita Kolesnew <chattrain.kol7@gmail.com>
2024-02-27 22:27:48 +0300

    refactor: changed image link

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -34,7 +34,7 @@ const LeftSidebar = () => {
         <div className="flex gap-4">
           <Link to={`/profile/${user.id}`} className="flex-center gap-3">
             <img
-              src={user.imageUrl || "/assets/images/profile-placeholder.svg"}
+              src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
               alt="Profile"
               className="h-14 w-14 rounded-full"
             />
